5988e36f62c5157e9e9e306d13477794070a1e71
Rediet Tekola <RDT7992@gmail.com>
2023-06-22 16:36:21 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmarks Management')
+    .setDescription('The bookmarks API description')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('explorer', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(4000);
 }
